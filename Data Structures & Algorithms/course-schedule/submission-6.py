3b0e3e2f2db8bd5438b2e49f140eb5ff11f23410
Tru Trong Bui <bui46@purdue.edu>
2026-04-03 13:11:06 -0400

    Add: course-schedule - submission-6

---
+++ Data Structures & Algorithms/course-schedule/submission-6.py
@@ -0,0 +1,39 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preMap = {i: [] for i in range(numCourses)}
+
+        for course, prereq in prerequisites:
+            preMap[course].append(prereq)
+
+        visited = set()
+
+        # dfs and return False if detect cycle (can't complete that course)
+        def dfs(course): 
+            if preMap[course] == []:
+                return True
+            if course in visited:
+                return False
+
+            visited.add(course)
+            for prereq in preMap[course]:
+                if not dfs(prereq):
+                    return False
+            
+            visited.remove(course)
+            preMap[course] = []
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+
+        return True
+            
+        
+        
+
+# numCourses = 2, prerequisites = [[0,1],[1,0]]
+# {0: [1], 1: [0]}
+
+# 0 --> 1
+#   <--
