df483daee784461e82d7e8e57ce47b37a473b239
Tru Trong Bui <bui46@purdue.edu>
2026-09-01 14:44:56 -0400

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        std::map<char, int> s_map{};
+        std::map<char, int> t_map{};
+
+        for (char &c : s) {
+            s_map[c] += 1;
+        }
+
+        for (char &c : t) {
+            t_map[c] += 1;
+        }
+
+        return (s_map == t_map);
+    }
+};
