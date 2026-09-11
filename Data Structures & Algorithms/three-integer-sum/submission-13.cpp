411b8ea25754b732c906f39aafe747f986f8c9b5
Tru Trong Bui <bui46@purdue.edu>
2026-09-11 14:40:32 -0400

    Add: three-integer-sum - submission-13

---
+++ Data Structures & Algorithms/three-integer-sum/submission-13.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+
+        vector<vector<int>> result{};
+        for (int curr_idx = 0; curr_idx < nums.size(); curr_idx++) {
+            if (curr_idx > 0 && nums[curr_idx] == nums[curr_idx - 1]) continue;
+
+            int l = curr_idx + 1;
+            int r = nums.size() - 1;
+
+            while (l < r) {
+                if (nums[l] + nums[r] < -nums[curr_idx]) {
+                    l += 1;
+                }
+                else if (nums[l] + nums[r] > -nums[curr_idx]) {
+                    r -= 1;
+                }
+                else {
+                    result.push_back(std::vector<int>{nums[curr_idx], nums[l], nums[r]});
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
+                    l++;
+                    r--;
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
+
+// curr = -4
+// [-4, -1, -1, 1, 3, 5, 5]
+//       l               r
+
+// [[-4, -1, 5], [-4, 3, 1]]
+
+
+
